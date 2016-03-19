75e9682ec7e22200aa51effe0d483977393b807d
Guilherme Leobas <guilhermeleobas@gmail.com>
2016-03-19 19:50:35 +0000

    change execution time from 600 to 60 seconds

--- gamixordenacaorwa-jogo/ga_maxrwa_estatico1/main.cc
+++ gamixordenacaorwa-jogo/ga_maxrwa_estatico1/main.cc
@@ -130,7 +130,7 @@ int main(int argc, char** argv) {
     oldstime = (double)(r.ru_stime).tv_sec+(r.ru_stime).tv_usec/1000000.0;
     oldcputime = oldutime + oldcputime;
  
-    best = static_cast<int>(ga.Execute(maxIterations, target, 600)); 
+    best = static_cast<int>(ga.Execute(maxIterations, target, 60)); 
     tempo = static_cast<double>(c1.dValue()); 
    
    getrusage(RUSAGE_SELF, &r);
